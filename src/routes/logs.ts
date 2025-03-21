d6c7a5a6db8e6b5a15a6c406f2cb7e546051fdcd
Felipe Silva <felipe.silva@assesi.com>
2025-03-21 05:51:44 -0300

    feat: statistics logs

--- src/routes/logs.ts
+++ src/routes/logs.ts
@@ -1,13 +1,35 @@
 import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
 
 import { db } from '../database/db';
 
 export async function logRoutes(app: FastifyInstance) {
+  app.get('/log/:id_campanha', async (request, reply) => {
+    const paramsSchema = z.object({
+      id_campanha: z.coerce.number(),
+    });
+    const { id_campanha } = paramsSchema.parse(request.params);
+
+    try {
+      const [result] = await db.query(
+        'SELECT A.descricao, COUNT(L.id_log) as quantidade FROM tab_log L INNER JOIN tab_campanha_acoes A ON (A.id_campanha_acao = L.id_acao) WHERE A.id_campanha = ? GROUP BY L.id_acao ORDER BY A.ordem ASC',
+        [id_campanha]
+      );
+
+      reply.code(200).send({ data: result });
+    } catch (err) {
+      console.error(err);
+      reply.status(500).send({ error: 'Erro ao buscar estatísticas da campanha' });
+    }
+  });
+
   app.post('/log', async (request, reply) => {
-    const { id_acao, link } = request.body as {
-      id_acao: number;
-      link: string;
-    };
+    const bodySchema = z.object({
+      id_acao: z.coerce.number(),
+      link: z.string().url()
+    });
+
+    const { id_acao, link } = bodySchema.parse(request.body);
 
     try {
       const [result] = await db.query(
