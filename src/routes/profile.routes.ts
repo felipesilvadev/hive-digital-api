d88e4fd731a0e9906a0f50c764a833e339ae4077
Felipe Silva <felipe.silva@assesi.com>
2025-03-26 08:22:18 -0300

    fix: user.sub jwtVerify

--- src/routes/profile.routes.ts
+++ src/routes/profile.routes.ts
@@ -5,7 +5,7 @@ import { verifyJWT } from '../middlewares/verify-jwt';
 export async function profileRoutes(app: FastifyInstance) {
   app.get('/me', { onRequest: [verifyJWT] }, async (request, reply) => {
     await request.jwtVerify();
-    console.log(request.user.sub);
+
     reply.code(200).send();
   });
 }
