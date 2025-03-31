5af72bc34f4b4f4753224b67a397d312ace859a8
Felipe Silva <felipe.silva@assesi.com>
2025-03-30 21:46:56 -0300

    refactor: actions quantity use case

--- src/use-cases/log/list-actions-quantity-by-campaign-use-case.ts
+++ src/use-cases/log/list-actions-quantity-by-campaign-use-case.ts
@@ -1,7 +1,6 @@
 import { RowDataPacket } from 'mysql2';
 
 import { db } from '../../database/db';
-import { AppError } from '../../errors/app-error';
 
 interface ListActionsQuantityByCampaignUseCaseRequest {
   id_campanha: number;
@@ -18,16 +17,17 @@ export class ListActionsQuantityByCampaignUseCase {
     id_campanha,
     date,
   }: ListActionsQuantityByCampaignUseCaseRequest) {
-    const filterDate = date ? date : new Date();
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    const filterDate = date
+      ? new Date(date).toISOString().split('T')[0]
+      : today.toISOString().split('T')[0];
 
     const [rows] = await db.query<QueryResult[]>(
-      /*sql*/ `SELECT A.descricao, COUNT(L.id_log) as quantidade FROM tab_log L INNER JOIN tab_campanha_acoes A ON (A.id_campanha_acao = L.id_acao) WHERE A.id_campanha = ? AND A.dt_delete IS NULL AND DATE(L.dt_insert) = DATE(?) GROUP BY L.id_acao ORDER BY A.ordem ASC`,
+      /*sql*/ `SELECT A.descricao, COUNT(L.id_log) as quantidade FROM tab_log L INNER JOIN tab_campanha_acoes A ON (A.id_campanha_acao = L.id_acao) WHERE A.id_campanha = ? AND A.dt_delete IS NULL AND DATE(L.dt_insert) = ? GROUP BY L.id_acao ORDER BY A.ordem ASC`,
       [id_campanha, filterDate],
     );
-
-    if (!rows.length) {
-      throw new AppError('Campanha não encontrada', 404);
-    }
 
     return rows;
   }
